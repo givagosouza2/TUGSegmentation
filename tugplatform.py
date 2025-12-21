c9fbce1f9bfd23b5c0837c03c5c490a4a5e2da56
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-12-21 11:30:25 -0300

    Update tugplatform.py

--- tugplatform.py
+++ tugplatform.py
@@ -256,7 +256,8 @@ with tabs[3]:
         st.info("Você precisa aceitar o consentimento e preencher a autoavaliação.")
         st.stop()
 
-    train_file = st.file_uploader("Arquivo de treino (txt/csv com ';')", type=["txt", "csv"], accept_multiple_files=False)
+    train_file = "Pct 01_GYR.txt"
+    #st.file_uploader("Arquivo de treino (txt/csv com ';')", type=["txt", "csv"], accept_multiple_files=False)
     if train_file is None:
         st.warning("Envie o arquivo de treino para continuar.")
         st.stop()
